4adcb6cfabe8e37f9c54ee3f48f492c69a373999
EdgardRamosB <edg_Ram@hotmail.com>
2025-09-16 11:46:19 -0500

    actualizando vercel3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-// import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
-
+import { Routes, Route, Navigate, BrowserRouter, HashRouter } from "react-router-dom";
 
 import Progreso from "./pages/Progreso";
 import Home from "./pages/Home";
@@ -12,15 +9,15 @@ import Servicios from "./componentes/Servicios";
 import Personalizado from "./componentes/Personalizado";
 import Navbar from "./componentes/Navbar";
 
+// Detectar si estamos en GitHub Pages (usa HashRouter) o en otro hosting (usa BrowserRouter)
+const isGitHubPages = window.location.hostname.includes("github.io");
+const Router = isGitHubPages ? HashRouter : BrowserRouter;
+
 export default function App() {
   return (
     <Router>
-      {/* 🔹 Menú de navegación */}
       <Navbar />
-
-      {/* 🔹 Rutas */}
       <Routes>
-        {/* ✅ Cuando alguien entre a "/", lo mandamos a "/rutina-app" */}
         <Route path="/" element={<Navigate to="/rutina-app" />} />
         <Route path="/rutina-app" element={<Home />} />
         <Route path="/progreso" element={<Progreso />} />
